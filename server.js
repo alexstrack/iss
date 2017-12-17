3587e751d404dab99275afa81f9a3c072d370738
Alexandro Strack <alexandrostrack@gmail.com>
2017-12-17 02:01:58 -0200

    Basic version calling the api and showing ISS position.

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-"use strict";
-
 const express = require('express');
+const request = require('request');
+const bodyparser = require('body-parser');
 const app = express();
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.get('/',function(req, res){
+    var url='http://api.open-notify.org/iss-now.json';
+    request(url, function(err, resp, body){
+        if (err) {
+            return console.log(err);
+        }
+        res.send(JSON.parse(body).iss_position);
+    });
+});
 
 app.listen(3000, function(err){
     if (err) {
-        console.log(err);
+        return console.log(err);
     }
-    else {
-        console.log('Server running on port 3000');
-    }
+    console.log('Server running on port 3000');
+    
 });
