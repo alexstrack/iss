b2c06ed8bfedddb20036d92b2e43805d55b57f8e
Alexandro Strack <alexandrostrack@gmail.com>
2017-12-21 22:27:01 -0200

    Index.ejs created based on signin exaple of bootsrtap.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const request = require('request');
 const bodyparser = require('body-parser');
+const path = require('path');
 const app = express();
 
 
+app.set('views', path.resolve(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
+
+app.get ('/login', function (req, res){
+    res.render('index', {message: "Funciona!"});
+});
 
 app.get('/',function(req, res){
     var url='http://api.open-notify.org/iss-now.json';
